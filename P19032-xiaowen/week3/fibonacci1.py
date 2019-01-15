2715b64960b29277a8fc471132c305760f479933
root <root@localhost.localdomain>
2019-01-15 14:00:30 +0800

    xiaowen's homework directory

---
+++ P19032-xiaowen/week3/fibonacci1.py
@@ -0,0 +1,16 @@
+#!/usr/local/python3/bin/python3
+n = int(input("enter number: "))
+if n < 0:
+    print("you must enter a number greater than zero")
+    exit(1)
+
+a = 1
+b = 1
+print(a)
+print(b)
+for i in range(2,n):
+    num = a+b
+    b=a
+    a=num
+    print(num)
+
