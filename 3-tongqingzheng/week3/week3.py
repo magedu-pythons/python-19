7b28aaf860e6c57104fc35e35f478ea79f88138a
Huangxiaoming <122749460@qq.com>
2019-01-19 23:50:06 +0800

    week3-3号选手-童庆政

---
+++ 3-tongqingzheng/week3/week3.py
@@ -0,0 +1,46 @@
+#ì³²¨ÄÇÆõº¯Êý1£º
+a = 1
+b = 1
+c = []
+while (1):
+    if (a <= 100):
+        if (a == 1):
+            c.append(a);
+            c.append(a);
+        else:
+            a = c[-1] + c[-2];
+            if (a < 100):
+                c.append(a);
+        a = a + 1;
+        b = b + 1;
+    else:
+        print(c);
+        break;
+
+#----------------------------------------
+
+#ì³²¨ÄÇÆõº¯Êý2£º
+a=[1,1]
+b=1
+while(1):
+        b = a[-1] + a[-2];
+        if(b<100):
+            a.append(b);
+        else:
+            break;
+print(a)
+
+#------------------------------------------
+
+#²»ÖØ¸´¼¤»îÂë
+import random
+a=[];
+while(1):
+    if(len(a)<200):
+        a.append(random.randrange(100000,999999))        
+        a= list(set(a))
+    else:
+        break;
+print(a);
+
+
