6123fa30430b8f0a4924889a26b72204b24f24d7
fuguang <fgwsq@foxmail.com>
2019-01-20 14:29:07 +0800

    add week3 homework

---
+++ 18-fuguang/week3/homework-3.py
@@ -0,0 +1,20 @@
+import random
+random_lst = []
+count = 0
+while True:
+    str = ''
+    count += 1
+    for i in range(5):
+        str1 = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+        str2 = random.choice(str1)
+        str += str2
+    if count > 100000:
+        break
+    if str in random_lst:
+        continue
+    else:
+        random_lst.append(str)
+    if len(random_lst) >= 200:
+        break
+for i in random_lst:
+    print(i)
