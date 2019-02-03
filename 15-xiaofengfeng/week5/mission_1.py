12b3a2fb32b77cd8aeeda2d3fb58319f96358659
Xiaofengfeng <34270470+lxfkid@users.noreply.github.com>
2019-02-03 17:15:56 +0800

    Homework

---
+++ 15-xiaofengfeng/week5/mission_1.py
@@ -0,0 +1,9 @@
+#1、问题描述：一个5位数，判断它是不是回文数。
+while True :
+    number = input("请输入一个5位数：")
+    number1 = number[:]
+    number2 = number[::-1]  #字符串取反
+    if number1 == number2 :
+        print("您输入的{}是回文数".format(number))
+    else:
+        print("您输入的{}不是回文数".format(number))
