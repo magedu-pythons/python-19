3996f4012068c495734c158ac1b1cb45ae228afb
listen2099 <listen2099@gmail.com>
2019-06-30 15:26:38 +0800

    ok

--- 6-kwei/week18/homework.py
+++ 6-kwei/week18/homework.py
@@ -109,3 +109,52 @@ if __name__ == '__main__':
 
 # 2、实现一个wsgi的http web服务端
 # 这块内容还没有学好，等学好了来补上
+from wsgiref.simple_server import make_server
+
+
+def simple_app(environ, start_response):  # 这两个参数是自动注入的
+    # print(environ)
+    for k, v in sorted(environ.items()):
+        print(k, v)
+    print('-' * 30)
+
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain; charset=utf-8')]
+
+    start_response(status, headers)  # 在返回正文之前首先要返回状态码,报文头
+
+    ret = [("%s: %s\n" % (key, value)).encode("utf-8")
+           for key, value in environ.items()]
+    return ret  # 这里return的是正文,必须是个可迭代对象,一般是列表,可以是一个字符串元素
+
+
+# 关于APP的其他写法A:
+class simple_app_A():
+    def __init__(self, name, age):
+        pass
+
+    def __call__(self, environ, start_response):
+        pass
+
+
+# 关于APP的其他写法B:
+class simple_app_B():
+    def __init__(self, environ, start_response):
+        for k, v in sorted(environ.items()):
+            print(k, v)
+        print('-' * 30)
+        status = '200 OK'
+        headers = [('Content-type', 'text/plain; charset=utf-8')]
+        start_response(status, headers)
+        self.ret = [("%s: %s\n" % (key, value)).encode("utf-8")
+                    for key, value in environ.items()]
+
+    def __iter__(self):
+        yield from self.ret
+
+
+# make_server('0.0.0.0', 9000, simple_app_A('tom', 20))
+# make_server('0.0.0.0', 9000, simple_app_B)
+with make_server('0.0.0.0', 9000, simple_app) as httpd:  # 撞见server
+    print("Serving on port 9000...")
+    httpd.serve_forever()
